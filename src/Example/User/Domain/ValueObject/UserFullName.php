c6383ffceb65e5cb2c3562b7fa5a0e0a0872c562
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-01-19 15:20:16 -0400

    Dominio: Value objects

---
+++ src/Example/User/Domain/ValueObject/UserFullName.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace Src\Example\User\Domain\ValueObject;
+
+final class UserFullName
+{
+    private $value;
+
+    public function __construct(array $value)
+    {
+        $this->value = $value;
+    }
+
+    public function value(): array
+    {
+        return $this->value;
+    }
+
+    public function firstName(): string
+    {
+        return $this->value['first_name'];
+    }
+
+    public function lastName(): string
+    {
+        return $this->value['last_name'];
+    }
+
+    public function fullName(): string
+    {
+        return $this->value['first_name'] . $this->value['last_name'];
+    }
+}
